9013bd5d3416aa0ae82c15a3a6c1f7469ff3a41d
Nachomontoya <montoyanacho@gmail.com>
2021-09-14 16:06:24 +0200

    middleware deleted

--- src/routes/api.php
+++ src/routes/api.php
@@ -21,10 +21,12 @@ use App\Http\Controllers\API\PropertyController;
 Route::post('login', [AuthController::class, 'signin']);
 Route::post('register', [AuthController::class, 'signup']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    // Route::resource('properties', PropertyController::class);
-    Route::apiResource('properties', PropertyController::class);
-});
+Route::apiResource('properties', PropertyController::class);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     // Route::resource('properties', PropertyController::class);
+//     Route::apiResource('properties', PropertyController::class);
+// });
     
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
