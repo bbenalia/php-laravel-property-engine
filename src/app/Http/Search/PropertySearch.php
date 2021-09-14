1beda93aa4a524c2d267877a6385ea6f7acc2c32
Brahim <brahimcasas@hotmail.es>
2021-09-14 17:59:20 +0200

    Fix: multi queries

--- src/app/Http/Search/PropertySearch.php
+++ src/app/Http/Search/PropertySearch.php
@@ -12,75 +12,61 @@ class PropertySearch
         $properties = new Property;
 
         // Search for a property based on their title.
-        // if ($filters->has('type_home')) {
-        //     $properties  = $properties->where('type_home', $filters->get('type_home'));
-        // }
-
-        // Search for a property based on their title.
-        if ($filters->has('q')) {
+        if ($filters->has('q'))
             $properties = $properties->where('city', 'LIKE', "%" . $filters->get('q') . "%");
-        }
+
 
         // rooms
         if ($filters->has('room'))
-            $properties = $properties->where('room', $filters->get('room'));
-
+            $properties = $properties->whereIn('room', $filters->get('room'));
         if ($filters->has('room_lte'))
-            $properties  = $properties->where('room', "<=", $filters->get('room_lte'));
-
+            $properties  = $properties->orWhere('room', "<=", $filters->get('room_lte'));
         if ($filters->has('room_gte'))
-        $properties  = $properties->where('room', ">=", $filters->get('room_gte'));
+            $properties  = $properties->orWhere('room', ">=", $filters->get('room_gte'));
 
         // bath
         if ($filters->has('bath'))
-        $properties = $properties->where('bath', $filters->get('bath'));
-
+            $properties = $properties->whereIn('bath', $filters->get('bath'));
         if ($filters->has('bath_lte'))
-        $properties  = $properties->where('bath', "<=", $filters->get('bath_lte'));
-
+            $properties  = $properties->orWhere('bath', "<=", $filters->get('bath_lte'));
         if ($filters->has('bath_gte'))
-        $properties  = $properties->where('bath', ">=", $filters->get('bath_gte'));
+            $properties  = $properties->orWhere('bath', ">=", $filters->get('bath_gte'));
 
         // type
+        // if ($filters->has('type'))
+        //     $properties = $properties->where('type', $filters->get('type'));
         if ($filters->has('type'))
-        $properties = $properties->where('type', $filters->get('type'));
+            $properties = $properties->whereIn('type', $filters->get('type'));
 
         // condition
         if ($filters->has('condition'))
-        $properties = $properties->where('condition', $filters->get('condition'));
+            $properties = $properties->whereIn('condition', $filters->get('condition'));
 
         // equipment
         if ($filters->has('equipment'))
-        $properties = $properties->where('equipment', $filters->get('equipment'));
+            $properties = $properties->where('equipment', $filters->get('equipment'));
 
         // more filters
         if ($filters->has('pets'))
-        $properties = $properties->where('pet', 1);
-
+            $properties = $properties->where('pet', 1);
         if ($filters->has('garden'))
-        $properties = $properties->where('garden', 1);
-
+            $properties = $properties->where('garden', 1);
         if ($filters->has('terrace'))
-        $properties = $properties->where('terrace', 1);
-
+            $properties = $properties->where('terrace', 1);
         if ($filters->has('air_conditioning'))
-        $properties = $properties->where('air_conditioning', 1);
-
+            $properties = $properties->where('air_conditioning', 1);
         if ($filters->has('swimming_pool'))
-        $properties = $properties->where('swimming_pool', 1);
+            $properties = $properties->where('swimming_pool', 1);
 
         // publication date
         if ($filters->has('publication_date_gte'))
-        $properties = $properties->where('created_at', '>=', $filters->get('publication_date_gte'));
+            $properties = $properties->where('created_at', '>=', $filters->get('publication_date_gte'));
 
-         // price
-         if ($filters->has('price_gte'))
-         $properties = $properties->where('price', '>=', $filters->get('price_gte'));
-
-         if ($filters->has('price_lte'))
-         $properties = $properties->where('price', '<=', $filters->get('price_lte'));
-
-
+        // price
+        if ($filters->has('price_gte'))
+            $properties = $properties->where('price', '>=', $filters->get('price_gte'));
+        if ($filters->has('price_lte'))
+            $properties = $properties->where('price', '<=', $filters->get('price_lte'));
 
         // Get the results and return them.
         return $properties->get();
